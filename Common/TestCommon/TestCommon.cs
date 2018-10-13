f7b58864c746cdcd82444b85a9d9413bf7b85984
Mobina Kashanian <Mobina.km@gmail.com>
2018-10-13 17:22:02 +0330

    some bugs just fixed

--- Common/TestCommon/TestCommon.cs
+++ Common/TestCommon/TestCommon.cs
@@ -1,5 +1,4 @@
-﻿using A2;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.IO;
