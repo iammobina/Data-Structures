840d8d14b31c3619a6e8312e0d407f3cd5644b69
Mobina Kashanian <Mobina.km@gmail.com>
2018-10-13 17:15:30 +0330

    A2 added

---
+++ A2/A2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FastMaxPairWiseProduct(new List<int> { 1,2,6});
+        }
+        
+        public static string Process(string Input)
+        {
+            var inData = Input.Split(new char[] { '\n', '\r', ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s))
+                .ToList();
+
+           // return NaiveMaxPairWiseProduct(inData).ToString();
+           return FastMaxPairWiseProduct(inData).ToString();
+
+        }
+
+        public static int NaiveMaxPairWiseProduct(List<int> numbers)
+        {
+            int Product = 0;
+            int n = numbers.Count();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i+1; j < n; j++)
+                {
+                    Product = Math.Max(Product, numbers[i] * numbers[j]);
+                }
+            }
+            return Product;
+        }
+        
+        public static int FastMaxPairWiseProduct(List<int> Numbers)
+        {
+            int n = Numbers.Count();
+            int i = 0;
+            int index1 = 0;
+          
+            for (; i < n; i++)
+            {
+                if (Numbers[i] >= Numbers[index1])
+                    index1=i ;
+            }
+            i = 0;
+            int index2 = 1;
+            for (; i < n; i++)
+            {
+                if (i != index1 && Numbers[i] >= Numbers[index2])
+                    index2=i;
+            }
+            return Numbers[index1] * Numbers[index2];
+        }
+
+    }
+}
+
---
+++ A2/A2Tests/ProgramTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestCommon;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace A2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void NaiveMaxPairWiseProductTest()
+        {
+            int TestAlgorithm = Program.NaiveMaxPairWiseProduct(new List<int>() { 1, 2, 9, 3, 8, 9, 4 });
+            Assert.AreEqual(TestAlgorithm, 81);
+        }
+        
+        [TestMethod()]
+        public void FastMaxPairWiseProductTest()
+        {
+            int TestAlgorithm = Program.FastMaxPairWiseProduct(new List<int>() { 1, 2,6 });
+            Assert.AreEqual(TestAlgorithm,12);
+        }
+        
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem("TestData", "A2_TestData")]
+        public void GradedTest_Performance()
+        {
+            TestCommon.TestTools.RunLocalTest("A2", Program.Process);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("TestData", "A2_TestData")]
+        public void GradedTest_Correctness()
+        {
+            TestCommon.TestTools.RunLocalTest("A2", Program.Process);
+        }
+
+        [TestMethod()]
+        public void GradedTest_Stress()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            List<int> A = new List<int>();
+            stopwatch.Start();
+            while (stopwatch.ElapsedMilliseconds <100)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(2, 10);
+                for (int i = 0; i < number; i++)
+                {
+                    Random R = new Random();
+                    A.Add(R.Next(0, 10));
+                }
+            }
+            var result1 = Program.NaiveMaxPairWiseProduct(A);
+            var result2 = Program.FastMaxPairWiseProduct(A);
+
+            Assert.AreEqual(result1, result2);
+        }
+        
+    }
+}
