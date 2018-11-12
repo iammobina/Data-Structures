000ac74f332f0fa33a75e22835d2ede4da341ed0
Mobina Kashanian <Mobina.km@gmail.com>
2018-11-12 10:16:22 +0330

    build won't failed plzzzzzzzzzzzzz

--- A6/A6/Program.cs
+++ A6/A6/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace A6
+﻿namespace A6
 {
     class Program
     {
--- A6/A6Tests/GradedTests.cs
+++ A6/A6Tests/GradedTests.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using A6;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TestCommon;
+using A6;
 
 namespace A6.Tests
 {
