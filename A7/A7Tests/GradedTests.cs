7477b8ec1396aa124a12398d61eec4115b24433b
Mobina Kashanian <Mobina.km@gmail.com>
2018-11-17 23:14:21 +0330

    A7

--- A7/A7Tests/GradedTests.cs
+++ A7/A7Tests/GradedTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using A7;
-using System;
-using System.Collections.Generic;
-using System.Linq;   
-using System.Text;
-using System.Threading.Tasks;
 using TestCommon;
 
 namespace A7.Tests
